10e87811e602734584d1d9cd950e060ddd84dc94
priyankas0907 <priyankasinha0301@gmail.com>
2023-08-03 18:09:00 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,22 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hello React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [React.createElement("div",
+        { id: "child" },
+        [React.createElement("h1", {}, "I am h1"),
+        React.createElement("h2", {}, "I am h2")]
+    ),
+    React.createElement("div",
+        { id: "child" },
+        [React.createElement("h1", {}, "I am h1"),
+        React.createElement("h2", {}, "I am h2")]
+    )
+    ]
+)
+console.log(parent);
+root.render(parent);
+
+
